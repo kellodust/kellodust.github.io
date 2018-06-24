9cdad950a9226fc5450137a0cc1fc676794d4138
Kelsie Dustin <kellodust@gmail.com>
2018-06-23 18:09:36 -0600

    Adding Javascript

--- cit_230/assignments/lesson-9/scripts/rainyday-9.js
+++ cit_230/assignments/lesson-9/scripts/rainyday-9.js
@@ -26,26 +26,40 @@
     function showData(jsonObj) {
         var cities = jsonObj['towns'];
 
-        for (var i = 0; i < cities.length; i++) {
+        for (var i = 0; i < cities.length; i++ ) {
+           if (i === 2) {
+                    continue;
+      }  
             var myArticle = document.createElement('article');
             var myH2 = document.createElement('h2');
             var myPara1 = document.createElement('p');
             var myPara2 = document.createElement('p');
             var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
+            var myPara5 = document.createElement('p');
+    /*            var myPara6 = document.createElement('p'); */
             var myList = document.createElement('ul');
 
             myH2.textContent = cities[i].name;
             myPara1.textContent = 'Motto: ' + cities[i].motto;
             myPara2.textContent = 'Year Founded: ' + cities[i].yearFounded;
-            myPara3.textContent = 'Population';
+            myPara3.textContent = 'Population: ' + cities[i].currentPopulation;
+            myPara4.textContent = 'Year Founded: ' + cities[i].yearFounded;
+            myPara5.textContent = 'Annual Rainfall: ' + cities[i].annualRainfall;
+  /*          myPara6.textContent = 'events: ' + cities[i].events; */
 
             
             myArticle.appendChild(myH2);
             myArticle.appendChild(myPara1);
             myArticle.appendChild(myPara2);
             myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+            myArticle.appendChild(myPara5);
+   /*             myArticle.appendChild(myPara6); */
             myArticle.appendChild(myList);
 
             section.appendChild(myArticle);
         }
     }
+
+document.getElementsById("cityfacts").innerHTML = n;
