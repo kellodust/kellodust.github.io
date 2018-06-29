e49cba4361e23a4a622724f1e244ba4a9dbe779e
Kelsie Dustin <kellodust@gmail.com>
2018-06-29 12:40:16 -0600

    error change

--- cit_230/assignments/weather-site/scripts/windchill.js
+++ cit_230/assignments/weather-site/scripts/windchill.js
@@ -8,4 +8,4 @@ var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,
 document.getElementById("windchill").innerHTML=f.toFixed(1);
 document.getElementById("temperature").innerHTML = t;
 document.getElementById("currentWind").innerHTML = s;
-//}
+//} 
